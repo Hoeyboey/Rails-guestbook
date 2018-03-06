e350b211449ea60da5066b12cfb8b7a302f21fa5
Hannah Dwan <hannahdwan96@gmail.com>
2018-03-06 14:23:26 +0000

    Tried to implement some tests, didn't work, managed to break loads of things, fixed now

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228112354) do
+ActiveRecord::Schema.define(version: 20180306134645) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20180228112354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "poster_name"
+  end
+
+  create_table "dummy_models", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
